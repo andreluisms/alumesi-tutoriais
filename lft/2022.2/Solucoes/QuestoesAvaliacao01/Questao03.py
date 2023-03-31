6be8aa330128981e41304c59dc9a0b2208825856
Andre Silva <andreluis.ms@gmail.com>
2023-03-31 15:36:28 -0300

    Update on Question3

--- lft/2022.2/Solucoes/QuestoesAvaliacao01/Questao03.py
+++ lft/2022.2/Solucoes/QuestoesAvaliacao01/Questao03.py
@@ -1,6 +1,6 @@
 import ply.lex as lex
 
-tokens = ['SELECT', 'FROM', 'JOIN', 'IF', 'THEN', 'PV', 'IGUAL', 'MAIS', 'TRUE', 'FALSE', 'NUM', 'ID']
+tokens = ['SELECT', 'FROM', 'JOIN', 'IF', 'THEN', 'PV', 'IGUAL', 'MAIS', 'TRUE', 'NUM', 'ID']
 
 t_SELECT = 'select'
 t_FROM = 'from'
@@ -11,7 +11,6 @@ t_PV = ';'
 t_IGUAL = '='
 t_MAIS = '\+'
 t_TRUE = 'true'
-t_FALSE = 'false'
 t_ID = 'id_[A-Za-z0-9]+'
 t_NUM = '0b[0-1]+'
 def t_blank(t):
@@ -19,7 +18,7 @@ def t_blank(t):
     pass
 
 lexer = lex.lex()
-input = 'select from join if then ; ; = + true false id_98 id_andre id_var 0b0101010010 0b1 0b0 0b001100'
+input = 'select from join if then ; ; = + true id_98 id_andre id_var 0b0101010010 0b1 0b0 0b001100'
 lexer.input(input)
 
 
